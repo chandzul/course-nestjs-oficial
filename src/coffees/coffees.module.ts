44fd7b69ceb6e6719d4df26b87c5857d2609b867
David Chandzul <dchandzul@gmail.com>
2021-08-16 18:14:02 -0500

    feat: Factory providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -9,12 +9,25 @@ import { COFFEE_BRANDS } from './coffees.constants';
 
 class MockCoffeesService {}
 
+class ConfigService {}
+
+class DevelopmentConfigService {}
+
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   // providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
   ],
   exports: [CoffeesService],
